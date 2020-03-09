786c7f000f8e21a67bfefaf9d3e44e925147515f
designerseyi <40296336+designerseyi@users.noreply.github.com>
2020-03-09 15:24:02 +0000

    Add files via upload

---
+++ pages/index.vue
@@ -0,0 +1,95 @@
+<template>
+    <paystack
+        :amount="amount"
+        :email="email"
+        :paystackkey="paystackkey"
+        :reference="reference"
+        :callback="callback"
+        :close="close"
+        :embed="false"
+    >
+       <i class="fas fa-money-bill-alt"></i>
+       Make Payment
+    </paystack>
+</template>
+
+<script type="text/javascript">
+
+import paystack from 'vue-paystack'
+
+
+
+
+export default {
+  components: {
+     paystack
+  },
+
+  data(){
+        return{
+          paystackkey: "pk_test_xxxxxxxxxxxxxxxxxxxxxxx", //paystack public key
+          email: "foobar@example.com", // Customer email
+          amount: 1000000 // in kobo
+        }
+    },
+
+    computed: {
+      reference(){
+        let text = "";
+        let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for( let i=0; i < 10; i++ )
+          text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+        return text;
+      }
+    },
+    methods: {
+      callback: function(response){
+        console.log(response)
+      },
+      close: function(){
+          console.log("Payment closed")
+      }
+    }
+
+}
+</script>
+
+<style>
+/* Sample `apply` at-rules with Tailwind CSS
+.container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+}
+*/
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
